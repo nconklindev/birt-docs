596f96f8424650c41bf79b05907b4c9b8279403e
nconklindev <190518646+nconklindev@users.noreply.github.com>
2024-12-03 14:03:18 -0500

    updated homepage items (WIP)

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Easy to Use",
+    title: "Data Dictionary",
     Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The Data Dictionary contains every entity available within Pro WFM for custom reporting with BIRT Studio or for use within Dataviews.
       </>
     ),
   },
   {
-    title: "Focus on What Matters",
+    title: "BIRT Reporting",
     Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The BIRT Studio documentation contains helpful articles on how to get started making reports within BIRT Studio as well as other advanced functionality.
       </>
     ),
   },
   {
-    title: "Powered by React",
+    title: "Dataviews",
     Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Our Dataview documentation will explore how to use the Data Dictionary to create the best Dataviews possible!
       </>
     ),
   },
